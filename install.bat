8ca4498669fe6e12457e941809bfa049db78efc5
Shuang Wu <seanwu1105@gmail.com>
2022-10-14 21:52:47 -0400

    build: install dependencies with install.bat (#4)
    * build: set vcpkgManifestInstall to false
    
    * build: install dependencies with install.bat

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@
 git clone https://github.com/Microsoft/vcpkg.git
 call .\vcpkg\bootstrap-vcpkg.bat
 .\vcpkg\vcpkg.exe integrate install
+.\vcpkg\vcpkg.exe install --triplet "x64-windows" "--x-install-root=vcpkg_installed\x64-windows\\"
 
 :: Install pre-commit hooks with Poetry
 
