5bd349ba21c0c860d5781c8be871e340c0706b6f
RaymondHuang210129 <raymond860909@gmail.com>
2023-04-26 23:33:14 -0400

    refactor: add position to camera statically

--- src/camera.h
+++ src/camera.h
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "shader.h"
+#include "user_control.h"
 
 static constexpr int numSurfaces = 10;
 static constexpr int numPointsOfSurface = 4;
@@ -24,10 +25,16 @@ public:
   CameraComponent(const glm::vec3& position);
   void render(const glm::mat4& view, const glm::mat4& proj,
               double horizontalAngleRadians, double verticalAngleRadians,
-              const glm::vec3& viewPosition, const glm::vec3& lightPosition);
+              const glm::vec3& viewPosition,
+              const glm::vec3& lightPosition) const;
+  void render(const glm::mat4& view, const glm::mat4& proj,
+              const glm::vec3& viewPosition, const glm::vec3& lightPosition,
+              UserControlData& userData) const;
+  static void updateUserControlData(const UserControlData& userData);
 
 private:
   static inline const LightingShaderProgramProvider shaderProgramProvider{};
   static inline const CameraVaoProvider vaoProvider{};
-  glm::mat4 model{1.0};
+  mutable glm::mat4 model{1.0};
+  static UserControlData userData;
 };
